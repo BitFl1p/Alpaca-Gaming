0bc979c8a7a65976a52df7faf722e2e04fdc95fa
Lumbr <jumiciobi@gmail.com>
2021-01-26 16:19:37 +0000

    I'm at the pizza hut, I'm at the taco bell, I'm at the combination pizza hut and taco bell

---
+++ Assets/Scripts/LoadFirstScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstScene : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void h()
+    {
+
+    }
+}
